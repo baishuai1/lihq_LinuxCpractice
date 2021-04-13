6f8e2ffe4886eee8a5921bc9a37f2458afafc039
baishuai <1693553104@qq.com>
2021-04-13 17:22:24 +0800

    添加有头结点的链表，移动顺序表的位置

--- list/sqlist/main.c
+++ list/sqlist/main.c
@@ -5,30 +5,49 @@
 
 int main(){
     int i,err;
-    sqlist *list;
+    sqlist *list = NULL,*list1 = NULL;
     datatype arr[] = {12,23,34,45,56};
-    //list = sqlist_create();
-    sqlist_create1(&list);
+    datatype arr1[] = {78,89,56,23,10};
+    list = sqlist_create();
+    list1 = sqlist_create();
+    //sqlist_create1(&list);
     if(list == NULL)
     {
         fprintf(stderr,"sqlist_create() failed!\n");
         exit(1);
     }
     for(i = 0 ; i < sizeof(arr)/sizeof(*arr) ; i++)
-        if(err = sqlist_insert(list,i,&arr[i]) != 0)
+    {
+        if(err = sqlist_insert(list,0,&arr[i]) != 0)
         {
-            if(err = -1)
+            if(err == -1)
                 fprintf(stderr,"sqlist_insert() -1 failed\n");
-            else if(err =-2)
+            else if(err == -2)
                 fprintf(stderr,"sqlist_insert() -2 failed\n");
             else
                 fprintf(stderr,"sqlist_insert()  failed\n");
-            exit(0);
+            exit(1);
         }
+    }
+    sqlist_display(list);
+    //int ret = 23;
+    for(i = 0 ; i < sizeof(arr1)/sizeof(*arr1) ; i++)
+        sqlist_insert(list1,0,&arr1[i]);
+
+    sqlist_display(list1);
+    sqlist_union(list,list1);
+    sqlist_display(list);
+
+#if 0
+    //ret = sqlist_find(list,&ret);
+    sqlist_delete(list,1);
+    /*if error*/
 
     sqlist_display(list);
-    int ret = 23;
-    //ret = sqlist_find(list,&ret);
+
+    sqlist_destory(list);
+#endif
+#if 0
     printf("the data loc is:%d\n",ret);    
     printf("list->last:%d\n",list->last);    
     for(i = 0;i < sizeof(arr)/sizeof(*arr); i++)
@@ -42,8 +61,10 @@ int main(){
    // sqlist_distory();
    ret =  sqlist_isempty(list);
    printf("isempty?:%d\n",ret);
-    
+#endif    
 
+    sqlist_destory(list);
+    sqlist_destory(list1);
 
     exit(0);
 }
--- list/sqlist/sqlist.c
+++ list/sqlist/sqlist.c
@@ -13,6 +13,7 @@ sqlist *sqlist_create()
 
     return me;
 }
+
 //用二级指针的原因是（因为此处我们想拿在手里进行操作的是一级指针
 //所以传它的地址也就是二级指针进取好进行值的回传)
 void sqlist_create1(sqlist **ptr)
@@ -23,6 +24,7 @@ void sqlist_create1(sqlist **ptr)
     (*ptr)->last = -1;
     return  ;
 }
+
 int sqlist_insert(sqlist *me,int i,datatype *data)
 {
     int j;
@@ -31,7 +33,7 @@ int sqlist_insert(sqlist *me,int i,datatype *data)
     if(i < 0 || i > me->last+1)
         return -2;
     
-    for(j = me->last ; i <= j; j++ )
+    for(j = me->last ; i <= j ; j-- )
         me->data[j+1] = me->data[j];    
 
     me->data[i] = *data;
@@ -39,6 +41,7 @@ int sqlist_insert(sqlist *me,int i,datatype *data)
 
     return 0;
 }
+
 int sqlist_delete(sqlist *me,int i)
 {
     int j;
@@ -52,29 +55,41 @@ int sqlist_delete(sqlist *me,int i)
     me->last--;
     return 0;
 }
+
 int sqlist_find(sqlist *me,datatype *data)
 {
     int i;
-    for(i = 0; i < me->last; i++)
+
+    if(sqlist_isempty(me) == 0)
+        return -1;
+
+    for(i = 0 ; i < me->last ; i++)
     {
         if(me->data[i] == *data)
-            return i+1;
+            return i;
     }
-    return -1;
+    return -2;
 }
+
 int sqlist_isempty(sqlist *me)
 {
-    return (me->last < 0 || me->last > (DATASIZE -1));
+    if(me->last == -1)
+        return 0;
+    return -1;
+        //(me->last < 0 || me->last > (DATASIZE -1));
 }
+
 int sqlist_setempty(sqlist *me)
 {
     me->last = -1;
     return 0;
 }
+
 int sqlist_getnum(sqlist *me)
 {
-    return me->last+1;
+    return (me->last+1);
 }
+
 void sqlist_display(sqlist *me)
 {
     int i;
@@ -89,16 +104,22 @@ void sqlist_display(sqlist *me)
 int sqlist_destory(sqlist *me)
 {
     free(me);
+    return 0;
 }
+
 int sqlist_union(sqlist *list1,sqlist *list2)
 {
     int i;
-    for(i = 0; i < list2->last;i++)
+//    if(list1->last == -1)
+//        return -1;
+//    if(list2->last == -1)
+//        return -2;
+
+    for(i = 0 ; i <= list2->last ; i++)
     {
-        if(sqlist_find(list1,list2->data[i]) < 0)
+        if(sqlist_find(list1,&list2->data[i]) < 0)
         {
-            if(sqlist_insert(list1,list1->last,list2->data[i]) !< 0);
-             return 0;
+            sqlist_insert(list1,0,&(list2->data[i]));
         }
     }
 }
