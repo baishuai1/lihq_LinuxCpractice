b8642dee1117acb7562f94e75c9672260097d034
baishuai <1693553104@qq.com>
2021-05-07 12:11:17 +0800

    Update sqlist.c
    fix mistake

--- list/sqlist/sqlist.c
+++ list/sqlist/sqlist.c
@@ -32,7 +32,7 @@ int sqlist_insert(sqlist *me,int i,datatype *data)
         return -1;
     if(i < 0 || i > me->last+1)
         return -2;
-    
+
     for(j = me->last ; i <= j ; j-- )
         me->data[j+1] = me->data[j];    
 
@@ -46,7 +46,7 @@ int sqlist_delete(sqlist *me,int i)
 {
     int j;
     //if(me->last == DATASIZE-1)
-      //  return -1;
+    //  return -1;
     if(i < 0 || i > me->last)
         return -1;
     for(j = i+1; j <= me->last;j++)
@@ -76,7 +76,7 @@ int sqlist_isempty(sqlist *me)
     if(me->last == -1)
         return 0;
     return -1;
-        //(me->last < 0 || me->last > (DATASIZE -1));
+    //(me->last < 0 || me->last > (DATASIZE -1));
 }
 
 int sqlist_setempty(sqlist *me)
